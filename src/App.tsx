1027d96aabbdfa8376485498e469654a92611362
kencyke <casek2703@gmail.com>
2019-12-20 03:13:01 +0900

    Add react-babylon example

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Engine, Scene, withBabylonJS } from 'react-babylonjs'
+import { Vector3 } from '@babylonjs/core';
+
+const EngineWithContext = withBabylonJS(Engine);
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <EngineWithContext antialias={false} adaptToDeviceRatio={true} canvasId="sample-canvas" width={1280} height={720}>
+        <Scene>
+          <freeCamera name="camera1" position={new Vector3(0, 5, -10)} setTarget={[Vector3.Zero()]} />
+          <hemisphericLight name="light1" intensity={0.7} direction={Vector3.Up()} />
+          <sphere name="sphere1" diameter={2} segments={16} position={new Vector3(0, 1, 0)} />
+        </Scene>
+      </EngineWithContext>
     </div>
+    
   );
 }
 
